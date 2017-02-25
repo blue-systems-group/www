25b31c06b21cb62b486ffd53efd7cec3653133ac
Geoffrey Challen <challen@buffalo.edu>
2017-02-24 16:03:32 -0800

    Header refactor.

--- src/assets/js/onload.js
+++ src/assets/js/onload.js
@@ -38,6 +38,16 @@ $(function() {
       ga('send', 'pageview', $(this).attr('href'));
     });
   });
+  if ($('#fb-root').length > 0) {
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.async = true;
+      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8";
+      fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+  }
 });
 
 // vim: ts=2:sw=2:et
